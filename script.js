64330a85129b488745b6cff85056f0f2792e25a6
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-21 22:43:05 +0200

    completed working on buttons

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const secretNumber = Math.ceil(Math.random()* 20)
-const input = document.querySelector('input').value;
+const input = document.querySelector('.guess');
 
 const number = document.querySelector('.number');
+let score = document.querySelector('.score').textContent = 20;
 
-number.textContent = input
 
-const message = document.querySelector('.message')
+check = document.querySelector('.check');
+let message = document.querySelector('.message')
 
-if(secretNumber === input){
-    message.textContent = 'yey you got it correct'
-}
-else if(secretNumber < input){
-    message.textContent ='your guess is high'
-}
-else if(secretNumber > input){
-    message.textContent = 'your guess is low'
-}
+check.addEventListener('click', function(){
+    
+    number.textContent = input.value;
+    if(secretNumber != input.value){
+        score -=1 
+ }
+    if(secretNumber == input.value){
+        message.textContent = 'yey you got it correct';
+    }
+    else if(secretNumber < input.value  && input.value  <= 20){
+        message.textContent ='your guess is high';
+    }
+    else if(input.value  > 20){
+        message.textContent = 'you are out of the range';
+    }
+    else if(secretNumber > input.value ){
+        message.textContent = 'your guess is low';
+    }
+})
+
+againButton = document.querySelector('.again');
+againButton.addEventListener('click',function(){
+    input.value = null;
+    number.textContent = input.value;
+    message.textContent = 'Start guessing...'
+
+})
+
+
