77be33aeb6ff2acaac4acfcff23f3f548483fd83
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-22 23:56:16 +0200

    completed the basic of the game

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-const secretNumber = Math.ceil(Math.random()* 20)
+let secretNumber = Math.ceil(Math.random()* 20)
 const input = document.querySelector('.guess');
 
 const number = document.querySelector('.number');
 let score = document.querySelector('.score');
 let scoreValue =20;
-let HighScore = document.querySelector('.HighScore').textContent = 20;
+
+let GameHighScore = 0;
 check = document.querySelector('.check');
 
 let message = document.querySelector('.message')
 
 check.addEventListener('click', function(){
-    
-    number.textContent = input.value;
-    if(secretNumber != input.value){
-        HighScore--;
-        scoreValue --
+   
+    scoreValue --
         score.textContent = scoreValue
-        if(scoreValue == 0){
-            message.textContent = 'Oops you lost'
-            check.disabled = true;
-                
-        }
- }
+        
+    number.textContent = input.value;
+    if(scoreValue == 0  || secretNumber ==input.value){
+        //message.textContent = 'Oops you lost'
+        check.disabled = true;
+            
+    }
+    
+ 
     if(secretNumber == input.value){
+        document.querySelector('body').style.backgroundColor = "blue";
         message.textContent = 'yey you got it correct';
+        if( scoreValue > GameHighScore){
+            GameHighScore = scoreValue
+            document.querySelector('.HighScore').textContent = GameHighScore;
+        }
+        
     }
     else if(secretNumber < input.value  && input.value  <= 20){
         message.textContent ='your guess is high';
@@ -38,6 +45,7 @@ check.addEventListener('click', function(){
 
 againButton = document.querySelector('.again');
 againButton.addEventListener('click',function(){
+    secretNumber = Math.ceil(Math.random()* 20)
     input.value = null;
     number.textContent = input.value;
     message.textContent = 'Start guessing...'
